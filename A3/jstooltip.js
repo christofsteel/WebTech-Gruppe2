a06607d70fd499199ce803c5c990def71095dd31
Hammy <christoph.stahl@uni-dortmund.de>
2011-11-05 17:58:53 +0100

    Aufgabe 3 funktioniert im chrome. andere Browser muss ich noch testen

--- A3/jstooltip.js
+++ A3/jstooltip.js
@@ -1,6 +1,40 @@
-function addTooltip(Id, seconds) {
-    var element=document.getElementById(Id);
-    var myDiv = document.createElement("div");
-    myDiv.setAttribute('id', 'tooltip');
-    document.appendChild(myDiv);
+timeout = new Array();
+function addTooltip(Id, tooltip,  milliseconds) {
+	var element=document.getElementById(Id);
+	tooltipId = "tooltip"+Id;
+
+	var tooltipdiv = document.createElement("div");
+	tooltipdiv.id=tooltipId;
+	tooltipdiv.innerHTML=tooltip;
+	tooltipdiv.style.position="absolute";
+	tooltipdiv.style.backgroundColor="#ffffff";
+	tooltipdiv.style.border="1px solid #72B0E6";
+	tooltipdiv.style.font="12px Verdana";
+	tooltipdiv.style.color="#1A80DB";
+	tooltipdiv.style.display="none";
+	document.body.appendChild(tooltipdiv);
+
+	element.setAttribute("onmouseover", "showtooltip('"+tooltipId+"', '"+tooltip+"',"+milliseconds+");");
+	element.setAttribute("onmousemove", "movetooltip('"+tooltipId+"');");
+	element.setAttribute("onmouseout", "hidetooltip('"+tooltipId+"');");
+}
+function showtooltip(tooltipId, tooltip, milliseconds) {
+	var e = window.event;
+	tooltipdiv = document.getElementById(tooltipId);
+	tooltipdiv.style.left=(e.clientX+10)+"px";
+	tooltipdiv.style.top=(e.clientY+10)+"px";
+	timeout[tooltipId]=window.setTimeout("tooltipdiv.style.display=\"block\"", milliseconds);
+}
+
+function hidetooltip(tooltipId) {
+	window.clearTimeout(timeout[tooltipId]);
+	tooltipdiv = document.getElementById(tooltipId);	
+	tooltipdiv.style.display="none";
+}
+function movetooltip(tooltipId) {
+	var e = window.event;
+	tooltipdiv = document.getElementById(tooltipId);
+	tooltipdiv.style.left=(e.clientX+10)+"px";
+	tooltipdiv.style.top=(e.clientY+10)+"px";
+
 }
